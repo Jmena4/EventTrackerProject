2d5abebe18435574727413c2b61b75af502a2ffc
Jesse <Jesse.mena4@gmail.com>
2019-04-01 17:44:19 -0600

    Updates to the FuelTracker objects

--- FuelMileageREST/src/main/java/com/skilldistillery/fuelmileage/repositories/FuelTrackerRepository.java
+++ FuelMileageREST/src/main/java/com/skilldistillery/fuelmileage/repositories/FuelTrackerRepository.java
@@ -1,10 +1,19 @@
 package com.skilldistillery.fuelmileage.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.fuelmileage.entities.FuelTracker;
+import com.skilldistillery.fuelmileage.entities.Vehicle;
 
 public interface FuelTrackerRepository extends JpaRepository<FuelTracker, Integer> {
+	
+//	@Query("SELECT v FROM Vehicle v JOIN FETCH v.fuel WHERE v.id = :vid")
+//	Vehicle queryForVehicleByFuelTrackerId(@Param("vid") Integer id);
+	
+	@Query("SELECT f FROM FuelTracker f JOIN FETCH f.vehicle WHERE f.id = :fid")
+	Vehicle queryForVehicleByFuelTrackerId(@Param("fid") Integer id);
 	
 	
 }
